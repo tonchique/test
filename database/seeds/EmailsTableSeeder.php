389eaf54ea7170d520790165f523152ef363792a
Anton Krekotun <antonkrekotun@gmail.com>
2018-07-20 01:22:47 +0300

    Added Email model, emails table migration, emails table seeder

--- database/seeds/EmailsTableSeeder.php
+++ database/seeds/EmailsTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class EmailsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-         $this->call(EmailsTableSeeder::class);
+        factory(App\Email::class, 10)->create();
     }
 }
