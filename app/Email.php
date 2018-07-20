b5d3928a627d8e51e775e4a1354eed0299ce1d0f
Anton Krekotun <antonkrekotun@gmail.com>
2018-07-20 23:08:00 +0300

    Added API;

--- app/Email.php
+++ app/Email.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
-    //
+    protected $fillable = ['name', 'value'];
+
+    public function toArray()
+    {
+        parent::toArray();
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'value' => $this->value,
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//List of all emails
+Route::get('emails', 'EmailController@index');
+
+//Create email entry
+Route::post('emails', 'EmailController@store');
